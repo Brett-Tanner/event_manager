66cf42d5893c0ee92f65d83325c3c62e4d954a9b
Brett Tanner <bretttanner171@gmail.com>
2022-07-20 16:27:51 +0900

    Implement my version of printing first names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,14 @@
-puts "#{File.exist? "event_attendees.csv"}"
+# if File.exist?("event_attendees.csv")
+#     contents = File.read("event_attendees.csv")
+#     puts contents
+# else
+#     puts "File not found"
+# end
 
-contents = File.read("event_attendees.csv")
-
-puts contents
+if File.exist?("event_attendees.csv")
+    attendee_array = File.readlines("event_attendees.csv")
+    attendee_array = attendee_array.map {|attendee| attendee.split(",")}
+    attendee_array.each {|attendee| puts attendee[2]}
+else
+    puts "File not found"
+end
