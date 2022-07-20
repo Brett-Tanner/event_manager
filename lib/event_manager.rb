ba0b143ccb12c04fcbb55c72d2ef6215167c5ee4
Brett Tanner <bretttanner171@gmail.com>
2022-07-20 16:46:01 +0900

    Update with single line if to not put header row

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,7 +8,7 @@
 if File.exist?("event_attendees.csv")
     attendee_array = File.readlines("event_attendees.csv")
     attendee_array = attendee_array.map {|attendee| attendee.split(",")}
-    attendee_array.each {|attendee| puts attendee[2]}
+    attendee_array.each_with_index {|attendee, index| puts attendee[2] if index != 0}
 else
     puts "File not found"
 end
