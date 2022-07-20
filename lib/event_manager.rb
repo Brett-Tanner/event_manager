458351acfe1e49b75ca6ee4786f0f40d5791922c
Brett Tanner <bretttanner171@gmail.com>
2022-07-20 16:53:02 +0900

    Convert headers to symbols used to access values

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,4 @@
+# Without parser
 # if File.exist?("event_attendees.csv")
 #     contents = File.read("event_attendees.csv")
 #     puts contents
@@ -5,10 +6,17 @@
 #     puts "File not found"
 # end
 
-if File.exist?("event_attendees.csv")
-    attendee_array = File.readlines("event_attendees.csv")
-    attendee_array = attendee_array.map {|attendee| attendee.split(",")}
-    attendee_array.each_with_index {|attendee, index| puts attendee[2] if index != 0}
-else
-    puts "File not found"
-end
+# if File.exist?("event_attendees.csv")
+#     attendee_array = File.readlines("event_attendees.csv")
+#     attendee_array = attendee_array.map {|attendee| attendee.split(",")}
+#     attendee_array.each_with_index {|attendee, index| puts attendee[2] if index != 0}
+# else
+#     puts "File not found"
+# end
+
+require 'csv'
+
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+# contents.each {|attendee| puts attendee[2]}
+
+contents.each {|attendee| puts attendee[:first_name]}
