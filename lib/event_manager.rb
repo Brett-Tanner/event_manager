5d35a8878909d30c1ec420f4b01d4abfcfea7436
Brett Tanner <bretttanner171@gmail.com>
2022-07-21 14:40:41 +0900

    Modularize getting the legislators

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,27 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
 def clean_zipcode(zipcode)
     zipcode = zipcode.to_s.dup.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zip(zip)
+    civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+    civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+    begin
+        legislators = civic_info.representative_info_by_address(
+            address: zip,
+            levels: 'country',
+            roles: ['legislatorUpperBody', 'legislatorLowerBody']
+        )
+        legislators = legislators.officials
+        legislator_names = legislators.map(&:name)
+        legislator_names.join(", ")
+    rescue Google::Apis::ClientError => e
+        # FIXME: This still prints the full error trace from the API
+        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
 end
 
 contents = CSV.open(
@@ -18,17 +34,7 @@ contents.each do |attendee|
     name = attendee[:first_name]
     zipcode = clean_zipcode(attendee[:zipcode])
 
-    begin
-        legislators = civic_info.representative_info_by_address(
-            address: zipcode,
-            levels: 'country',
-            roles: ['legislatorUpperBody', 'legislatorLowerBody']
-        )
-        legislators = legislators.officials
-    rescue
-        legislators = 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-    end
+    legislators = legislators_by_zip(zipcode)
     
-
     puts "#{name}: #{zipcode} #{legislators}"
 end
