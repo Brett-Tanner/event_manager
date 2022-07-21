f94ebba84249c1895a2d3035acbddc01cec77ad1
Brett Tanner <bretttanner171@gmail.com>
2022-07-21 14:08:47 +0900

    Handle errors from empty zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,33 +1,34 @@
-# Without parser
-# if File.exist?("event_attendees.csv")
-#     contents = File.read("event_attendees.csv")
-#     puts contents
-# else
-#     puts "File not found"
-# end
-
-# if File.exist?("event_attendees.csv")
-#     attendee_array = File.readlines("event_attendees.csv")
-#     attendee_array = attendee_array.map {|attendee| attendee.split(",")}
-#     attendee_array.each_with_index {|attendee, index| puts attendee[2] if index != 0}
-# else
-#     puts "File not found"
-# end
-
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
-contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
-# contents.each {|attendee| puts attendee[2]}
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 def clean_zipcode(zipcode)
     zipcode = zipcode.to_s.dup.rjust(5, "0")[0..4]
 end
 
+contents = CSV.open(
+    "event_attendees.csv", 
+    headers: true, 
+    header_converters: :symbol
+)
+
 contents.each do |attendee|
     name = attendee[:first_name]
-    zipcode = attendee[:zipcode]
+    zipcode = clean_zipcode(attendee[:zipcode])
+
+    begin
+        legislators = civic_info.representative_info_by_address(
+            address: zipcode,
+            levels: 'country',
+            roles: ['legislatorUpperBody', 'legislatorLowerBody']
+        )
+        legislators = legislators.officials
+    rescue
+        legislators = 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
     
-    zipcode = clean_zipcode(zipcode)
-   
-    puts "#{name}: #{zipcode}"
+
+    puts "#{name}: #{zipcode} #{legislators}"
 end
