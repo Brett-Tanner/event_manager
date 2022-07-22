78a64388617721f12d6146ae28d3507a840000fa
Brett Tanner <bretttanner171@gmail.com>
2022-07-22 13:15:42 +0900

    Refactor to use .erb template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
-
-template_letter = File.read("form_letter.html")
+require 'erb'
 
 def clean_zipcode(zipcode)
     zipcode = zipcode.to_s.dup.rjust(5, "0")[0..4]
@@ -16,13 +15,10 @@ def legislators_by_zip(zip)
             address: zip,
             levels: 'country',
             roles: ['legislatorUpperBody', 'legislatorLowerBody']
-        )
-        legislators = legislators.officials
-        legislator_names = legislators.map(&:name)
-        legislator_names.join(", ")
+        ).officials
     rescue Google::Apis::ClientError => e
         # FIXME: This still prints the full error trace from the API
-        'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+        "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
     end
 end
 
@@ -32,14 +28,15 @@ contents = CSV.open(
     header_converters: :symbol
 )
 
+template_letter = ERB.new(File.read("form_letter.erb"))
+
 contents.each do |attendee|
     name = attendee[:first_name]
     zipcode = clean_zipcode(attendee[:zipcode])
 
     legislators = legislators_by_zip(zipcode)
-    
-    personal_letter = template_letter.gsub("FIRST_NAME", name)
-    personal_letter.gsub!("LEGISLATORS", legislators)
+
+    personal_letter = template_letter.result(binding)
 
     puts "#{personal_letter}"
 end
