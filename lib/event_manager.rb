90db09a04225ed01666df5191b2e2096759bfaa1
Brett Tanner <bretttanner171@gmail.com>
2022-07-24 12:06:28 +0900

    Print how many people signed up on each day

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -64,6 +64,21 @@ def print_reg_times(reg_times)
     puts "#{time_sentences.join("\n")}"
 end
 
+def print_reg_days(reg_days)
+    sorted_days = reg_days.sort_by(&:last).reverse
+    # use to transform days from int without long case
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    day_sentences = sorted_days.map do |array|
+        array[0] = days[array[0]]
+        if array[1] == 1
+            "#{array[1]} person signed up on #{array[0]}."
+        else
+            "#{array[1]} people signed up on #{array[0]}."
+        end
+    end
+    puts "#{day_sentences.join("\n")}"
+end
+
 contents = CSV.open(
     "event_attendees.csv", 
     headers: true, 
@@ -72,8 +87,9 @@ contents = CSV.open(
 
 template_letter = ERB.new(File.read("form_letter.erb"))
 
-# store the frequency of registration for each time
+# store the frequency of registration for each time adn day
 reg_times = Hash.new(0)
+reg_days = Hash.new(0)
 
 contents.each do |attendee|
     # info for letter
@@ -85,6 +101,7 @@ contents.each do |attendee|
 
     reg_datetime = time?(attendee[:regdate])
     reg_times[reg_datetime.hour] += 1
+    reg_days[reg_datetime.wday] += 1
 
     legislators = legislators_by_zip(zipcode)
 
@@ -94,3 +111,4 @@ contents.each do |attendee|
 end
 
 print_reg_times(reg_times)
+print_reg_days(reg_days)
